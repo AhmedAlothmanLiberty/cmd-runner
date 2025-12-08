9d6103dbbc33a13987a87a362997ff7f8340fed2
AHMED ALOTHMAN <ahmed@libertydebtrelief.com>
2025-12-09 00:05:34 +0300

    Test => liberty-cmd/reports:dev-features

--- config/database.php
+++ config/database.php
@@ -100,17 +100,13 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'host' => env('CMD_DB_HOST', 'localhost'),
+            //            'port' => env('DB_PORT', '1433'),
+            'database' => env('CMD_DB_DATABASE', 'forge'),
+            'username' => env('CMD_DB_USERNAME', 'forge'),
+            'password' => env('CMD_DB_PASSWORD', ''),
+            'trust_server_certificate' => env('DB_CMD_TRUST_CERT', false),
+            'charset' => 'utf8',
         ],
 
     ],
@@ -148,7 +144,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
